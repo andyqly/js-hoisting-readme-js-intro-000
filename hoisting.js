e02430732cb579f22c0ccc08a28f2aa32b122e64
Andy L <andyqly@hotmail.com>
2019-10-31 16:23:13 +0000

    Automatically backed up by Learn

--- hoisting.js
+++ hoisting.js
@@ -1,4 +1,14 @@
+function hoisting
+console.log("before all")
 
+function callMe() {
+  var lyric = "maybe";
+  console.log("I just met you...");
+  console.log("and this is crazy..");
+  console.log("but here's my number..");
+  console.log("so call me");
+  return lyric;
+}
 
 function crazy() {
   // fix the code in here:
@@ -9,15 +19,6 @@ function crazy() {
   }
 }
 
-function callMe() {
-  var lyric = "maybe";
-  console.log("I just met you...");
-  console.log("and this is crazy..");
-  console.log("but here's my number..");
-  console.log("so call me");
-  return lyric;
-
-}
 
 
 
